1d8afaeb66bbebccd66b86660582829bc8b7aaec
pkarlovsek <peter.karlovsek@cosylab.com>
2021-10-07 11:16:16 +0200

    Update main template.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,84 @@
 #include "PegSolitaire.hpp"
+
 #include <iostream> 
+#include <chrono>
 
-int main(){
+int main()
+{
+  using namespace std::chrono;
+  auto const english_board = ps::get_english_board();
 
-  auto const english_board = get_english_board();
+  std::cout << "Start solving:\n";
+  ps::print(english_board);
+  std::cout << "---------------\n";
 
-  print(english_board); // english_board->print(); ??
-  std::cout << "Peg count: " << english_board.peg_count() << "\n";
+  auto const start = high_resolution_clock::now();
 
-// Output
-//
-//   ···
-//   ···
-// ·······
-// ···o···
-// ·······
-//   ···
-//   ···
-//
-// Peg count: 32  
+  // Solving board e.g.
+  // 
+  // Add your code
+  // 
+  // ps::solve(english_board, moves); or
+  // const auto moves = ps::solve(english_board);
+
+  auto end = high_resolution_clock::now();
+  auto duration_ms = duration_cast<duration<double, std::milli>>(end - start).count();
+
+  // Print moves
+  //
+  // Add your code
+  // e.g.
+  // ps::print_moves(results);
+  std::cout << "Solving took: " << duration_ms << " ms\n";
+
+  /*
+  Output example:
+  ~~~~~~~~~~~~~~~~
+  Start solving:
+  111
+  111
+1111111
+1110111
+1111111
+  111
+  111
+---------------
+  111
+  111
+1111111
+1110111
+1111111
+  111
+  111
+
+  111
+  111
+1111111
+1111001
+1111111
+  111
+  111
+
+[ ... omitted ...]
+
+  000
+  000
+0000000
+0000000
+0001000
+  010
+  000
+
+  000
+  000
+0000000
+0000000
+0000000
+  000
+  010
+
+Solving took: 492.057 ms
+  */
+
   return 0;
 }
